23047ae6bb1a43fff5d77e07de38560b855e6b62
NandoSchlemper <bkschlemper@gmail.com>
2025-08-15 11:33:22 -0300

    [release] 0.0.1v

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"athena/infrastructure/api"
 	"athena/repository"
 	"athena/services"
-	"fmt"
 	"log"
 	"time"
 
@@ -20,26 +19,17 @@ func main() {
 	cfg := api.NewTrackerAPIConfig(30 * time.Second)
 	cfg.SetDefaultTracker()
 	client := api.NewTrackerAPIClient(cfg)
-	resp, err := client.ListaVeiculos()
-	if err != nil {
-		log.Fatal("Erro ao receber resposta da aplicação: %w", err)
-	}
-
-	if resp != nil {
-		fmt.Printf("Resposta recebida")
-	}
 
 	dbrepo := repository.NewMongoDB(1, 20, 1)
 	db, err := dbrepo.InitDB()
 
 	if err != nil {
-		log.Fatal("Erro ao iniciar o banco de dados: %w", err)
+		log.Fatal("Erro ao iniciar o banco de dados. %w", err)
 	}
 
 	tracker_repo := repository.NewTrackerRepository(db)
 	tracker_service := services.NewTrackerService(tracker_repo, client)
-	if err = tracker_service.SaveTrackerData(); err != nil {
-		log.Fatal("Erro ao salvar os dados através do serviço Tracker: %w", err)
-	}
 
+	application := services.NewTimerService(tracker_service, client)
+	application.StartApplication()
 }
--- services/timer_service.go
+++ services/timer_service.go
@@ -1,1 +1,50 @@
 package services
+
+import (
+	"athena/infrastructure/api"
+	"fmt"
+	"log"
+	"time"
+)
+
+type ITimerService interface {
+	StartApplication()
+}
+
+type TimerService struct {
+	trackerService ITrackerService
+	api            api.ITrackerAPIClient
+}
+
+// StartApplication implements ITimerService.
+func (t *TimerService) StartApplication() {
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		<-ticker.C
+		resp, err := t.api.ListaVeiculos()
+		if err != nil {
+			log.Fatal("Erro ao receber resposta da aplicação: %w", err)
+		}
+
+		if resp != nil {
+			fmt.Printf("Resposta recebida")
+		}
+
+		if err = t.trackerService.SaveTrackerData(); err != nil {
+			log.Fatal("Erro ao salvar os dados através do serviço Tracker: %w", err)
+		}
+	}
+
+}
+
+func NewTimerService(
+	trackerService ITrackerService,
+	api api.ITrackerAPIClient,
+) ITimerService {
+	return &TimerService{
+		trackerService: trackerService,
+		api:            api,
+	}
+}

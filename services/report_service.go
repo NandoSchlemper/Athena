3d4268059a0280f8bfc85a61b413f7174387f0a6
NandoSchlemper <bkschlemper@gmail.com>
2025-08-18 17:13:50 -0300

    [fix] report service vai ser feito na CLI, pq eu quero.

--- services/report_service.go
+++ services/report_service.go
@@ -1,1 +1,11 @@
 package services
+
+import "athena/repository"
+
+type IReportService interface {
+	SendReportsByEmail()
+}
+
+type ReportService struct {
+	databaseRepository repository.ITrackerRepository
+}
